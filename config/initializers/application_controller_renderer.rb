02200ce5d835ab153a053a9b1ab6f50ca3c72b8e
AyumiM5 <mmmiray.555@gmail.com>
2022-01-05 07:34:36 +0000

    [Fix]header

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'e84fd45e042150dbab2d4f8f794109fd62f5dd1ab0f152122d49d31d62ca7b62ab79c0f4d34b89a22de469e200733268f746872ec782fa5990832d6b47fea356'
