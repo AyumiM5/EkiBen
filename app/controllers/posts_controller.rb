08c203e6d7693ba18de29bb5eb109a3249c69ca6
AyumiM5 <mmmiray.555@gmail.com>
2022-01-08 14:27:56 +0000

    [Add]PostNewIndex

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,27 @@
 class PostsController < ApplicationController
-  def indew
+  def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = current_user.posts.new(post_params)
+    @post.save
+    redirect_to user_path(current_user)
   end
 
   def edit
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
