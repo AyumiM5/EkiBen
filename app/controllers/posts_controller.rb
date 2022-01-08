de356e0cf6273169bcc6310d89d85439a3fa0c9c
AyumiM5 <mmmiray.555@gmail.com>
2022-01-08 14:10:02 +0000

    [Add]PostController

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+  def indew
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  get 'posts/indew'
+  get 'posts/show'
+  get 'posts/new'
+  get 'posts/edit'
   get 'users/show'
   devise_for :users
   root to: 'homes#top'
   
   resources :users, only: [:show, :edit, :update]
+  resources :posts
 end
