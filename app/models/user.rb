883ab78d74d15f07ffc15213e5ca6874168508e8
AyumiM5 <mmmiray.555@gmail.com>
2022-01-08 14:14:04 +0000

    [Add]PostModel

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   attachment :profile_image
+  has_many :posts, dependent: :destroy
   
   validates :name, uniqueness: true, presence: true, length: { minimum: 2, maximum: 10 }, format: { with: /\A[a-zA-Z0-9]+\z/ }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_081538) do
+ActiveRecord::Schema.define(version: 2022_01_08_141139) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
