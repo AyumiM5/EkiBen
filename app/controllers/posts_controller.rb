569bf1f8f8673ae48ed3c22ef7dd6386d9c3cc7b
AyumiM5 <mmmiray.555@gmail.com>
2022-01-09 13:42:42 +0000

    [Add]PostEditUpdate

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,13 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to user_path(current_user)
   end
   
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    @posts = current_user.posts
   end
   
   def edit
